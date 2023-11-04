2d61ff681f942a279b392415401153f498383570
Tejas Weldode <112164797+Tejas-Weldode@users.noreply.github.com>
2023-11-04 16:10:11 +0530

    completed the orderhistory  page

--- orderhistory.php
+++ orderhistory.php
@@ -13,61 +13,59 @@ if (!isset($_SESSION['email'])) {
         <link href="css/style.css" rel="stylesheet">
         <script src="js/jquery.js"></script>
         <script src="js/bootstrap.min.js"></script>
+        <style>
+            .productName{
+                color:rgb(18, 170, 18);
+            }
+        </style>
     </head>
     <body>
-        <div class="container-fluid" id="content">
+        <div class="container" id="content">
             <?php include 'includes/header.php'; ?>
-            <div class="col-lg-4 col-md-6 ">
-                    <img src="img/cart.jpg" style="float: left;">
-                </div>
-            <div class="row decor_bg">
-                <div class="col-md-6">
-                    <table class="table table-striped">
-    
-                        <!--show table only if there are items added in the cart-->
-                        <?php
-                        $sum = 0;$id='';
-                        $user_id = $_SESSION['user_id'];
-                        $query = "SELECT items.price AS Price, items.id As id, items.name AS Name  FROM user_item JOIN items ON user_item.item_id = items.id WHERE user_item.user_id='$user_id' and `status`=2";
-                        $query1="SELECT user_item.date_time AS Timedate from user_item WHERE user_id='$user_id' and `status`=2";
-                        $result = mysqli_query($con, $query)or die($mysqli_error($con));
-                        $result1 = mysqli_query($con, $query1)or die($mysqli_error($con));
-                        if (mysqli_num_rows($result) >= 1) {
-                            ?>
-                            <h1 style="margin-bottom: 20px; font-weight: 20;"><center>Order History</center></h1>
-                            <thead>
-                                <tr>
-                                    <th>Item name</th>
-                                    <th>Price</th>
-                                    <th>Order & time</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
-                                $total = 0;
-                                while ($row = mysqli_fetch_array($result)) {
-                                    $id .= $row["id"] . ", ";
-                                    echo '<tr><td><a href="order.php">'. $row["Name"] . "</a></td><td>Rs. " . $row["Price"] . "</td>";
-                                    $total= $total + $row["Price"];
-                                    while ($row1 = mysqli_fetch_array($result1) ) {
-                                    echo"<td>" . $row1["Timedate"] . "</td></tr>"; 
-                                    break;
-                                }
-                                }
-                                echo "<tr><td>Total</td>"."<td>Rs. ".$total."</td></tr>";
-                                ?>
-                            </tbody>
+                <table class="table table-striped">
+
+                    <!--show table only if there are items added in the cart-->
+                    <?php
+                    $sum = 0;$id='';
+                    $user_id = $_SESSION['user_id'];
+                    $query = "SELECT items.price AS Price, items.id As id, items.name AS Name  FROM user_item JOIN items ON user_item.item_id = items.id WHERE user_item.user_id='$user_id' and `status`=2";
+                    $query1="SELECT user_item.date_time AS Timedate from user_item WHERE user_id='$user_id' and `status`=2";
+                    $result = mysqli_query($con, $query)or die($mysqli_error($con));
+                    $result1 = mysqli_query($con, $query1)or die($mysqli_error($con));
+                    if (mysqli_num_rows($result) >= 1) {
+                        ?>
+                        <!-- <h1 style="margin-bottom: 20px; font-weight: 20;"><center>Order History</center></h1> -->
+                        <thead>
+                            <tr>
+                                <th>Item name</th>
+                                <th>Price</th>
+                                <th>Order & time</th>
+                            </tr>
+                        </thead>
+                        <tbody>
                             <?php
-                        } else {
-                            echo "Sorry! No orders placed yet";
-                        }
-                        ?>
+                            $total = 0;
+                            while ($row = mysqli_fetch_array($result)) {
+                                $id .= $row["id"] . ", ";
+                                echo '<tr><td><a class="productName" href="order.php">'. $row["Name"] . "</a></td><td>Rs. " . $row["Price"] . "</td>";
+                                $total= $total + $row["Price"];
+                                while ($row1 = mysqli_fetch_array($result1) ) {
+                                echo"<td>" . $row1["Timedate"] . "</td></tr>"; 
+                                break;
+                            }
+                            }
+                            echo "<tr><td>Total</td>"."<td>Rs. ".$total."</td></tr>";
+                            ?>
+                        </tbody>
                         <?php
-                        ?>
-                    </table>
-                </div>
+                    } else {
+                        echo "Sorry! No orders placed yet";
+                    }
+                    ?>
+                    <?php
+                    ?>
+                </table>
             </div>
-        </div>
         <?php include("includes/footer.php"); ?>
     </body>
 </html>
